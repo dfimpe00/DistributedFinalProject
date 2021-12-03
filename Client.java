e9e368469f9ffb3c4d5cee3b130032e84bbc2838
Daegan Fimpel <70106916+dfimpe00@users.noreply.github.com>
2021-12-03 09:59:12 -0600

    Create Client.java
    sends file to servers

---
+++ Client.java
@@ -0,0 +1,49 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+
+	public final static int SOCKET_PORT = 52005;
+	public final static String SERVER = "localhost";
+	public final static String FILE_TO_SEND = "test22.txt";
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		OutputStream os = null;
+
+		Socket sock = null;
+		try {
+			sock = new Socket(SERVER, SOCKET_PORT);
+			System.out.println("Connecting...");
+
+			// send file
+			File myFile = new File(FILE_TO_SEND);
+			byte[] mybytearray = new byte[(int) myFile.length()];
+			
+			fis = new FileInputStream(myFile);
+			bis = new BufferedInputStream(fis);
+			
+			bis.read(mybytearray, 0, mybytearray.length);
+			os = sock.getOutputStream();
+			
+			System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+			os.write(mybytearray, 0, mybytearray.length);
+			os.flush();
+			System.out.println("Done.");
+		} finally {
+			if (bis != null)
+				bis.close();
+			if (os != null)
+				os.close();
+			if (sock != null)
+				sock.close();
+
+		}
+	}
+
+}
