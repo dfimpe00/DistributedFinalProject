734a10c447e3ee7447a265e5d8edb5c63723ab51
Daegan Fimpel <70106916+dfimpe00@users.noreply.github.com>
2021-12-13 01:38:05 -0600

    Update MasterServer.java

--- MasterServer.java
+++ MasterServer.java
@@ -1,186 +1,40 @@
-import java.io.BufferedOutputStream;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class MasterServer {
 
-	private static HashMap<String, HashMap<String, String[]>> fileLog = new HashMap<String, HashMap<String, String[]>>();
-	static ArrayList<String> nodeList = new ArrayList<String>();
-	public static String INSTRUCTION_FILE = "instruction.txt";
-	public static String FILE_TO_BE_RECEIVED;
-	public static int FILE_SIZE = 6022386;
+	public static HashMap<String, HashMap<String, String[]>> fileLog = new HashMap<String, HashMap<String, String[]>>();
+	public static HashMap<String, String[]> dataNodes = new HashMap<String, String[]>();
+	public static HashMap<String, String> up = new HashMap<String, String>();
 
 	public static void main(String[] args) throws IOException {
 
-		int bytesRead;
-		int current = 0;
-		FileOutputStream fos = null;
-		BufferedOutputStream bos = null;
-
 		int port = 52005;
+		readfile("password.txt");// loads the userpassword Hashmap from a text file
+		loadFileLog();// loads the FileLog from a text file
 
-		MasterClient masterClient;
+		try {
+			ServerSocket ss = new ServerSocket(port);
+			System.out.println("Starting server");
 
-		ServerSocket server = new ServerSocket(port);
+			while (true) {
 
-		System.out.println("Server is starting...");
-		String isFive = "";
-		String tokens1 = "";
-		String tokens2 = "";
-		String isValid = "";
+				Socket client = ss.accept();
+				System.out.println("New client connected" + client.getInetAddress().getHostAddress());
+				MasterThread mt = new MasterThread(client, fileLog, dataNodes, up);
 
-		while (true) {
-
-			try {
-				masterClient = new MasterClient(server.accept());
-
-				Thread t = new Thread(masterClient);
-
+				Thread t = new Thread(mt);
 				t.start();
-
-				if (isFive.equals("5")) {
-
-					if (MasterClient.checkPass(tokens1, tokens2)) {
-						isValid = "1";
-					} else {
-						isValid = "2";
-					}
-
-					ObjectOutputStream oos = new ObjectOutputStream(masterClient.client.getOutputStream());
-
-					oos.writeObject(isValid);
-
-					isFive = "";
-					tokens1 = "";
-					tokens2 = "";
-					oos.close();
-
-				}
-
-				try {
-
-					System.out.println("Accepted connection : " + masterClient.client);
-
-					byte[] mybytearray = new byte[FILE_SIZE];
-					InputStream is = masterClient.client.getInputStream();
-
-					fos = new FileOutputStream(INSTRUCTION_FILE);
-					bos = new BufferedOutputStream(fos);
-
-					bytesRead = is.read(mybytearray, 0, mybytearray.length);
-					current = bytesRead;
-
-					do {
-						bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
-						if (bytesRead >= 0)
-							current += bytesRead;
-					} while (bytesRead > -1);
-
-					bos.write(mybytearray, 0, current);
-					bos.flush();
-
-					System.out.println("File " + INSTRUCTION_FILE + " downloaded (" + current + " bytes read)");
-
-				} finally {
-					if (fos != null)
-						fos.close();
-					if (bos != null)
-						bos.close();
-				}
-
-				try {
-
-					FileReader fr = new FileReader(INSTRUCTION_FILE);
-					BufferedReader br = new BufferedReader(fr);
-
-					String line;
-					String[] tokens = null;
-
-					while ((line = br.readLine()) != null) {
-
-						tokens = line.split(",");
-						// sending ip from nodes case
-						if (tokens[0].equals("1")) {
-							String ip = tokens[1];
-							nodeList.add(ip);
-
-						}
-						// inserting file case, tokens[0] is instruciton, tokens[1] is username
-						else if (tokens[0].equals("2")) {
-
-							for (int i = 0; i < nodeList.size(); i++) {
-								MasterClient.sendFiletoNode(nodeList.get(i), INSTRUCTION_FILE);
-							}
-
-						}
-						// sets filename being received to filename sent specified by user, tokens[1] is
-						// filename, set global variable to tokens[1]
-						else if (tokens[0].equals("3")) {
-							FILE_TO_BE_RECEIVED = tokens[1];
-							for (int i = 0; i < nodeList.size(); i++) {
-								MasterClient.sendFileName(nodeList.get(i), FILE_TO_BE_RECEIVED);
-
-							}
-						}
-						// delete a file case, tokens[0] is instruction, tokens[1] is username,
-						// tokens[2] is filename being deleted
-						else if (tokens[0].equals("4")) {
-							String filetoDelete = tokens[2];
-							for (int i = 0; i < nodeList.size(); i++) {
-								MasterClient.sendDeleteFileName(nodeList.get(i), filetoDelete);
-
-							}
-						}
-						// user authentication
-						else if (tokens[0].equals("5")) {
-
-							isFive = tokens[0];
-							tokens1 = tokens[1];
-							tokens2 = tokens[2];
-
-						}
-					}
-				} catch (Exception ex) {
-
-				}
-
-				String message = "Thread " + t.getName() + " ";
-				System.out.println(message);
-
-			} catch (Exception ex) {
-
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-	}
-
-	public static String[] getFileList(String username) {
-
-		Set<String> set = fileLog.get(username).keySet();
-		String[] fileList = new String[set.size()];
-
-		int i = 0;
-		for (String x : set) {
-			fileList[i] = x;
-			i++;
-		}
-		return fileList;
 	}
 
 	// loads in values to fileLog from the masterfiles.txt
@@ -211,157 +65,41 @@ public class MasterServer {
 				}
 
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
-	// adds the entered values into the fileLog hashmap with the keys being the
-	// username and filename
-	public static synchronized void logAdd(String username, String filename, String date, String path, String server) {
-
-		String[] datePathServer = { date, path, server };
-
-		// if key doesn't exist add it and make the HashMap for the value
-		if (!fileLog.containsKey(username)) {
-			HashMap<String, String[]> innermap = new HashMap<String, String[]>();
-			innermap.put(filename, datePathServer);
-			fileLog.put(username, innermap);
-
-			try {
-				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("masterfiles.txt", true)));
-				out.println(username + "::" + filename + "::" + date + "::" + path + "::" + server);
-				out.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		} else if (fileLog.get(username).containsKey(filename)) {
-
-		} else {
-			fileLog.get(username).put(filename, datePathServer);
-			try {
-				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("masterfiles.txt", true)));
-				out.println(username + "::" + filename + "::" + date + "::" + path + "::" + server);
-				out.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	// prints the contents of the fileLog hashmap to masterfiles.txt
-	public static synchronized void printlog() {
+	public static void readfile(String filename) {
 		try {
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("masterfiles.txt", false)));
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
 
-			for (Map.Entry<String, HashMap<String, String[]>> set1 : fileLog.entrySet()) {
-				for (Map.Entry<String, String[]> set2 : set1.getValue().entrySet()) {
-					out.println(set1.getKey() + " " + set2.getKey() + " " + set2.getValue()[0] + " "
-							+ set2.getValue()[1] + " " + set2.getValue()[2]);
+			String line = "";
+			while ((line = br.readLine()) != null) {
+
+				String[] parts = line.split(",");
+
+				String name = parts[0];
+				String passWord = parts[1];
+
+				// put name, number in HashMap if they are
+				// not empty
+				if (!name.equals("") && !passWord.equals("")) {
+
+					up.put(name, passWord);
 				}
 			}
 
-			out.close();
-		} catch (IOException e) {
+			br.close();
+
+		} catch (Exception e) {
+
 			e.printStackTrace();
 		}
 
 	}
 
-	// Prints the contents of the fileLog hashmap to the console
-	public static void PrintFileLog() {
-		for (Map.Entry<String, HashMap<String, String[]>> set1 : fileLog.entrySet()) {
-			for (Map.Entry<String, String[]> set2 : set1.getValue().entrySet()) {
-				System.out.println(set1.getKey() + " " + set2.getKey() + " " + set2.getValue()[0] + " "
-						+ set2.getValue()[1] + " " + set2.getValue()[2]);
-			}
-		}
-	}
-
-	// removes logged file from fileLog hashmap and updates the log text file
-	public static void logRemove(String username, String filename) {
-		if (fileLog.containsKey(username)) {
-			if (fileLog.get(username).containsKey(filename)) {
-				fileLog.get(username).remove(filename);
-				printlog();
-			}
-		}
-
-	}
-
-	// below here are methods for system logs and appending to the system logs
-	public static synchronized void sysLogPrint(String str) {
-		try {
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("log.txt", true)));
-			out.println(str);
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void dnconnect(String nodeName, String ip) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[dncconnect]";
-		String str2 = datetime + ", " + nodeName + ", " + ip;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void userauth(String userName, String ip) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[userauth]";
-		String str2 = datetime + ", " + userName + ", " + ip;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void userconnect(String userName, String ip) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[userconnect]";
-		String str2 = datetime + ", " + userName + ", " + ip;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void dnfailure(String dnName, String ip) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[dnfailure]";
-		String str2 = datetime + ", " + dnName + ", " + ip;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void fileinsert(String dnName, String userName, String fileName) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[fileinsert]";
-		String str2 = datetime + ", " + dnName + ", " + userName + ", " + fileName;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void filedelete(String dnName, String userName, String fileName) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[filedelete]";
-		String str2 = datetime + ", " + dnName + ", " + userName + ", " + fileName;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void fileaccess(String dnName, String userName, String fileName) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[fileaccess]";
-		String str2 = datetime + ", " + dnName + ", " + userName + ", " + fileName;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
-
-	public static void filefailure(String dnName, String userName, String fileName) {
-		LocalDateTime datetime = LocalDateTime.now();
-		String str1 = "[filefailure]";
-		String str2 = datetime + ", " + userName + ", " + fileName;
-		String str3 = String.format("%-15s %s", str1, str2);
-		sysLogPrint(str3);
-	}
 }
