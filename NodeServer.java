2c2b1bee2adf62783c0fdb40b768b60c4f00e81c
Cloud28 <39338419+Cloud28@users.noreply.github.com>
2021-12-13 12:08:26 -0600

    Add files via upload

--- NodeServer.java
+++ NodeServer.java
@@ -1,16 +1,23 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NodeServer {
 	
+	public static HashMap<String, HashMap<String, String[]>> fileLog = new HashMap<String, HashMap<String, String[]>>();
+	
 	public static void main(String[] args) {
 		int port = 52005;
-		String NAME = "DATA1";
+		String NAME = "DATA2";
 		
 		SendNode(NAME);
+		loadFileLog();
 		
 		try {
 			ServerSocket ss = new ServerSocket(port);
@@ -20,7 +27,7 @@ public class NodeServer {
 				Socket client = ss.accept();
 				System.out.println("New client connected: " + client.getInetAddress().getHostAddress());
 				
-				NodeThread nt = new NodeThread(client);
+				NodeThread nt = new NodeThread(client, fileLog);
 				Thread t = new Thread(nt);
 				t.start();
 				
@@ -32,7 +39,7 @@ public class NodeServer {
 	
 	public static void SendNode(String name) {
 		
-		String host = "192.168.1.196";
+		String host = "10.181.244.157";
 		int port = 52005;
 
 		try {
@@ -53,5 +60,39 @@ public class NodeServer {
 		}
 		
 	}
+	
+	//loads in values to fileLog from the datafiles.txt
+		public static void loadFileLog() {
+				
+				String[] logLine;
+				String username;
+				String filename;
+				// 
+				try (BufferedReader br = new BufferedReader(new FileReader("datafiles.txt"))) {
+				    String line;
+				    while ((line = br.readLine()) != null && br.readLine()!=" ") {
+				       logLine = line.split("::");
+				       if(logLine.length == 6) {
+				    	   username = logLine[0];
+					       filename = logLine[1];
+					       String[] datePathSizeAccess = {logLine[2], logLine[3], logLine[4], logLine[5]};
+					       
+					       //if key doesn't exist add it and make the HashMap for the value
+					       if(!fileLog.containsKey(username)) {
+					    	   HashMap<String, String[]> innermap =  new HashMap<String, String[]>();
+					    	   innermap.put(filename, datePathSizeAccess);
+					    	   fileLog.put(username, innermap);
+					       }else {
+					    	   fileLog.get(username).put(filename, datePathSizeAccess);
+					       }
+
+				       }
+				       		       
+				    }
+				}catch(Exception e){
+					e.printStackTrace();	
+				}
+			}
 
 }
+
--- NodeThread.java
+++ NodeThread.java
@@ -1,21 +1,30 @@
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NodeThread extends Thread {
-
+	
+	
+	static HashMap<String, HashMap<String, String[]>> fileLog = new HashMap<String, HashMap<String, String[]>>();
 	Socket s;
 	int bytesRead;
 	public static int FILE_SIZE = 6022386;
 
-	public NodeThread(Socket s) {
+	public NodeThread(Socket s, HashMap<String, HashMap<String, String[]>> fileLog) {
 		this.s = s;
+		this.fileLog = fileLog;
 	}
 
 	public void run() {
@@ -39,6 +48,7 @@ public class NodeThread extends Thread {
 				String filename = tokens[2];
 				String contents = tokens[3];
 				File dir = new File(username + "/");
+				logInsert(username, filename, LocalDateTime.now() +"", username+"/", contents.length() + "", "0");
 
 				if (dir.isDirectory()) {
 					File f = new File(username + "/" + filename);
@@ -63,7 +73,7 @@ public class NodeThread extends Thread {
 
 					Files.deleteIfExists(Paths.get(username + "/" + filename));
 					System.out.println("File: " + username + "/" + filename + " was successfully deleted");
-
+					logDelete(username, filename);
 				}
 			} else if (tokens[0].equals("6")) {
 				// file retrieve
@@ -74,4 +84,78 @@ public class NodeThread extends Thread {
 		}
 
 	}
+	
+	//adds the entered values into the fileLog hashmap with the keys being the username and filename
+		public static synchronized void logInsert(String username, String filename, String date, String path, String size, String accessnum) {
+				
+				String[] datePathServer = {date,path,size, accessnum};
+				
+				//if key doesn't exist add it and make the HashMap for the value
+			    if(!fileLog.containsKey(username)) {
+			    	HashMap<String, String[]> innermap =  new HashMap<String, String[]>();
+			    	innermap.put(filename, datePathServer);
+			    	fileLog.put(username, innermap);
+			    	
+			    	try {
+					    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("datafiles.txt", true)));
+					    out.println(username + "::" + filename + "::" + date + "::" + path + "::" + size+"::" + accessnum);
+					    out.close();
+					} catch (IOException e) {
+					    e.printStackTrace();
+					}
+			    	
+			    }else if(fileLog.get(username).containsKey(filename)) {
+			    	
+			    }else {
+			    	fileLog.get(username).put(filename, datePathServer);
+			    	try {
+					    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("datafiles.txt", true)));
+					    out.println(username + "::" + filename + "::" + date + "::" + path + "::" + size+"::" + accessnum);
+					    out.close();
+					} catch (IOException e) {
+					    e.printStackTrace();
+					}
+			    }	
+			}
+		
+		public static void logDelete(String username, String filename) {
+			if(fileLog.containsKey(username)) {
+				if(fileLog.get(username).containsKey(filename)) {
+					fileLog.get(username).remove(filename);
+					printlog();
+				}
+			}
+		
+	}
+
+		
+		//prints the contents of the fileLog hashmap to datafiles.txt
+		public static synchronized void printlog() {
+			try {
+			    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("datafiles.txt", false)));
+			  
+			    for(Map.Entry<String, HashMap<String, String[]>> set1 : fileLog.entrySet()) {
+			    	for(Map.Entry<String, String[]> set2 : set1.getValue().entrySet()) {
+			    		out.println(set1.getKey() + " " + set2.getKey() + " " + set2.getValue()[0]+ " " + set2.getValue()[1]+ " " + set2.getValue()[2] + " " + set2.getValue()[3]);
+			    	}
+			    }
+			
+			    out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+					
+			
+		}
+		
+		//adds one to the number of time accessed and then rewrites the datafiles.txt
+		public static synchronized void accessedFile(String user, String file) {
+			String[] hashvalue = fileLog.get(user).get(file);
+			int numAccessed = Integer.parseInt(hashvalue[5]);
+			numAccessed += 1;
+			hashvalue[5] = numAccessed + ""; 
+			fileLog.get(user).put(file, hashvalue);
+			printlog();
+		}
+		
 }
